164fff37e5b9dd3eecf0de57ea1ad3cff8c7cfda
HarryAmmon <harry_a99@hotmail.co.uk>
2021-05-09 08:45:08 +0100

    Create recipie feed files

---
+++ views/RecipeFeed/RecipeFeed.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const RecipeFeed: React.FC = () => {
+  return (
+    <View>
+      <Text>Recipe Feed</Text>
+    </View>
+  );
+};
--- views/index.ts
+++ views/index.ts
@@ -1,3 +1,4 @@
 export * from "./Login/Login";
 export * from "./Home/Home";
 export * from "./Registration/Registration";
+export * from "./RecipeFeed/RecipeFeed";
